6fedf8188b642a882305ab7daeaa83590c7be3ff
jhonbertel123 <jhonbertel123@gmail.com>
2024-12-11 13:32:50 -0500

    Cambiios

--- models/categoriaModels.js
+++ models/categoriaModels.js
@@ -5,12 +5,6 @@ const categoriaSchema = new mongoose.Schema({
     descripcion: { type: String },
     estado: { type: String, enum: ['activo', 'inactivo'], required: true },
     observaciones: { type: String },
-    calificacion: { 
-        type: Number, 
-        min: 1, 
-        max: 5, 
-        required: true 
-    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Categoria', categoriaSchema);
